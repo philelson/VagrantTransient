e585d0a050e84fc60794012b91b09595d1e90ab1
Phil Elson <phil@pegasus-commerce.com>
2015-06-25 11:20:43 +0100

    Upped the issue due to the package changes

--- src/VagrantTransient.php
+++ src/VagrantTransient.php
@@ -57,7 +57,7 @@ class VagrantTransient extends Command
     /**
      * This is the application version
      */
-    const VERSION               = "0.1.1";
+    const VERSION               = "0.2.0";
 
     /**
      * This is the default file where the _environments are stored.
